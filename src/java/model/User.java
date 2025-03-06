c9e900d0c36f013d0207467e8440dc15170dfdab
Thu-410 <minhthu.4102004@gmail.com>
2025-03-06 21:47:21 +0700

    function: add register, login, logout, changepass

---
+++ src/java/model/User.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package model;
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/**
+ *
+ * @author Minh Thu
+ */
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String passwordHash;
+    private String role;
+
+    public User() {
+    }
+
+    public User(int id, String username, String email, String passwordHash, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+    
+}
